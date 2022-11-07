c4a09587a60be5a52ce8ad514386136dec25b379
Makc300 <117684797+Makc300@users.noreply.github.com>
2022-11-08 00:32:04 +0400

    Add files via upload

---
+++ Адресная книга/src/mocks/mocks.js
@@ -0,0 +1,36 @@
+const Mocks = () => {
+    const mocks = [{
+        "id": 1,
+        "name": 'Харитонов Павел',
+        "phone": '+7(906)154-11-14',
+        "favorite": 'favorite'
+    },
+    {
+        "id": 2,
+        "name": 'Авдеев Виктор',
+        "phone": '+7(906)523-22-85',
+        "favorite": 'none'
+    },
+    {
+        "id": 3,
+        "name": 'Суханова Ксения',
+        "phone": '+7(928)754-33-85',
+        "favorite": 'none'
+    },
+    {
+        "id": 4,
+        "name": 'Давыдов Александр',
+        "phone": '+7(906)432-44-73',
+        "favorite": 'none'
+    },
+    {
+        "id": 5,
+        "name": 'Горбунов Артём',
+        "phone": '+7(906)634-55-24',
+        "favorite": 'favorite'
+    }]
+
+    return mocks
+}
+
+export default Mocks
